c22f796e996aab6eedc277aabf8cf96ba777f224
Sudipta Sinha Shanta <shantonu2050@gmail.com>
2022-12-17 03:30:09 +0600

    All done

--- src/Components/Quizes/Quizes.js
+++ src/Components/Quizes/Quizes.js
@@ -7,7 +7,7 @@ const Quizes = () => {
   const quizes = quizeLoader.data;
   console.log(quizes);
   return (
-    <div className="grid gap-4 grid-cols-4 p-4 m-10">
+    <div className="grid gap-4 md:grid-cols-4 p-4 m-10">
       {quizes.map((quizcard) => {
         return <QuizCard key={quizcard.id} quizcard={quizcard}></QuizCard>;
       })}
